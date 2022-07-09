2e60d75881cec2bb087ba96b2cf94781025a667d
Maxwell Rosenberg <max@maxrosenb.com>
2022-07-09 18:26:07 -0400

    fix viewport mobile

--- app/root.tsx
+++ app/root.tsx
@@ -56,6 +56,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="viewport" content="width=500, initial-scale=1" />
         <Meta />
         <Links />
       </head>

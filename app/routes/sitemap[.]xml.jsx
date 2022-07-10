2a3f7a400c913f7df5229379eba1ca59d872eb39
Maxwell Rosenberg <max@maxrosenb.com>
2022-07-10 16:14:28 -0400

    fix sitemap

--- app/routes/sitemap[.]xml.jsx
+++ app/routes/sitemap[.]xml.jsx
@@ -1,4 +1,41 @@
+const express = require("express");
+const contentful = require("contentful");
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { createGzip } = require("zlib");
+const { ContentfulSitemap } = require("contentful-sitemap");
+import "dotenv/config";
+
+const client = contentful.createClient({
+  accessToken: process.env.CONTENTFUL_CDA_TOKEN,
+  space: process.env.CONTENTFUL_SPACE,
+});
+
+const sitemapGenerator = new ContentfulSitemap(client, [
+  {
+    pattern: "/",
+    id: "[HOME_PAGE_ID]",
+  },
+  {
+    pattern: "/blog/:slug",
+    priority: 0.8,
+    query: {
+      content_type: "blogPost",
+      select: "fields.slug",
+    },
+    params: {
+      slug: "fields.slug",
+    },
+  },
+]);
+
 export const loader = () => {
+  let sitemap;
+  const smStream = new SitemapStream({ hostname: "https://maxrosenb.com/" });
+  //   console.log("sm stream:");
+  //   console.log(smStream);
+  const pipeline = smStream.pipe(createGzip());
+  //   console.log("Pipeline:");
+  //   console.log(pipeline);
   // handle "GET" request
   // separating xml content from Response to keep clean code.
   const content = `
@@ -11,12 +48,37 @@ export const loader = () => {
     </urlset>
     `;
   // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
-  return new Response(content, {
-    status: 200,
-    headers: {
-      "Content-Type": "application/xml",
-      "xml-version": "1.0",
-      encoding: "UTF-8",
-    },
+  return sitemapGenerator.buildRoutes().then((routes) => {
+    // console.log("routes:", routes);
+    // console.log(routes);
+    // pipe your entries or directly write them.
+    routes.forEach((route) => smStream.write(route));
+    smStream.end();
+
+    // cache the response
+    const result = streamToPromise(pipeline).then((sm) => {
+      console.log("SM:");
+      console.log(sm);
+      sitemap = sm;
+      console.log("sitemap:");
+      console.log(sitemap);
+      const res = new Response(sitemap, {
+        status: 200,
+        headers: {
+          "Content-Type": "application/xml",
+          "xml-version": "1.0",
+          "Content-Encoding": "gzip",
+        },
+      });
+      return res;
+      console.log("RES:");
+      console.log(res);
+    });
+    return result;
+    // stream write the response
+
+    // pipeline.pipe(res).on("error", (e) => {
+    //   throw e;
+    // });
   });
 };

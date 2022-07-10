9e48f63e131eb21c883b4b6df307db6c1d2da981
Maxwell Rosenberg <max@maxrosenb.com>
2022-07-10 02:10:23 -0400

    css fix

--- app/routes/blog.$post.tsx
+++ app/routes/blog.$post.tsx
@@ -108,7 +108,7 @@ export default function BlogPost() {
             }}
           >
             {post.author?.profilepic && (
-              <div>
+              <div className="figureContainer">
                 <figure className={"image"}>
                   <picture>
                     <div>

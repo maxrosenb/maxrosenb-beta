5e46b6044351589f91b59089d0518e6f44512f43
Maxwell Rosenberg <max@maxrosenb.com>
2022-07-10 16:16:10 -0400

    add SEO stuff

--- app/routes/sitemap[.]xml.jsx
+++ app/routes/sitemap[.]xml.jsx
@@ -29,39 +29,17 @@ const sitemapGenerator = new ContentfulSitemap(client, [
 ]);
 
 export const loader = () => {
-  let sitemap;
   const smStream = new SitemapStream({ hostname: "https://maxrosenb.com/" });
-  //   console.log("sm stream:");
-  //   console.log(smStream);
   const pipeline = smStream.pipe(createGzip());
-  //   console.log("Pipeline:");
-  //   console.log(pipeline);
-  // handle "GET" request
-  // separating xml content from Response to keep clean code.
-  const content = `
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-    <loc>https://www.maxrosenb.com/</loc>
-    <lastmod>2022-07-09T00:15:16+01:00</lastmod>
-    <priority>1.0</priority>
-    </url>
-    </urlset>
-    `;
+
   // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
   return sitemapGenerator.buildRoutes().then((routes) => {
-    // console.log("routes:", routes);
-    // console.log(routes);
     // pipe your entries or directly write them.
     routes.forEach((route) => smStream.write(route));
     smStream.end();
 
     // cache the response
-    const result = streamToPromise(pipeline).then((sm) => {
-      console.log("SM:");
-      console.log(sm);
-      sitemap = sm;
-      console.log("sitemap:");
-      console.log(sitemap);
+    const result = streamToPromise(pipeline).then((sitemap) => {
       const res = new Response(sitemap, {
         status: 200,
         headers: {
@@ -71,14 +49,12 @@ export const loader = () => {
         },
       });
       return res;
-      console.log("RES:");
-      console.log(res);
     });
     return result;
     // stream write the response
 
-    // pipeline.pipe(res).on("error", (e) => {
-    //   throw e;
-    // });
+    pipeline.pipe(res).on("error", (e) => {
+      throw e;
+    });
   });
 };
